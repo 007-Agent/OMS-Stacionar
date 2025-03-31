e0bbe137b0ab0c665492df08b8d2a779d08a7def
Абаничев Даниил Александрович <AbanichevDaniil@kdpmc.ru>
2025-03-31 16:11:08 +0300

    31/03-n3

--- src/hosp/PacienList/PacientList.jsx
+++ src/hosp/PacienList/PacientList.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { PacientType } from "../../components/calls/pacientType";
+
 import { SpicolList } from "../../components/PacientItem/SpicolList";
+
 import "./pacientList.scss";
 
 export const PacientList = () => {
   return (
     <>
       <div className="pacient__content">
-        <PacientType />
         <SpicolList />
         <input type="text" style={{ width: "50px", height: "50px" }} />
       </div>
